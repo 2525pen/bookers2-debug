a681e369a685d3b7fa9023ef92c550ab43cfff8d
2525pen <sakai3ons@gmail.com>
2020-02-24 08:24:30 +0000

    [start]最初のコミットです

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resource :favorites, only: [:create, :destroy]
+  resource :post_comments, only: [:create, :destroy]
   get 'home/about'
 end
