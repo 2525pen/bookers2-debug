392a3f9b47e0add746415ae261608ef4475d9464
2525pen <sakai3ons@gmail.com>
2020-02-25 10:56:16 +0000

    [start]最初のコミットです

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+  	resources :relation_ships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books do
 	   resource :favorites, only: [:create, :destroy]
 	   resource :post_comments, only: [:create, :destroy]
